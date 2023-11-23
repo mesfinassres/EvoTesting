3fbb0e19b8014fce8b51134da9e61a1468f2b498
mesfinassres <mesfin.assres@gmail.com>
2023-11-23 12:55:07 +0100

    New

---
+++ src/WebJUnitTest.java
@@ -0,0 +1,37 @@
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebJUnitTest {
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void openBrowser()
+    {
+        System. setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void assertURL() {
+        // TODO Auto-generated method stub
+        driver.get("https://www.lambdatest.com/");
+
+        String actualURL = driver.getCurrentUrl();
+        System.out.println(actualURL);
+
+        Assert.assertEquals("https://www.lambdatest.com/",actualURL);
+        System.out.println("Test Passed");
+    }
+
+    @AfterClass
+    public static void closeBrowser()
+    {
+        driver.close();
+    }
+
+}
